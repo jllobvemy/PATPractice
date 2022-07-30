a8fa16960e7afcf41baece2a83ebc3d6ded0904a
Jllobvemy <jllobvemy@gmail.com>
2022-07-30 18:37:33 +0800

    A1093

---
+++ A1093CountPATs/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long answer = 0;
+string PAT;
+
+int main() {
+    cin >> PAT;
+    long long PNum = 0, ANum = 0, PANum = 0;
+    for (char it : PAT) {
+        if (it == 'P') {
+            PNum++;
+        }
+        else if (it == 'A' && PNum > 0) {
+            ANum++;
+            PANum += PNum;
+        } else if (PANum > 0){
+            answer += PANum;
+            answer %= 1000000007;
+        }
+    }
+    cout << answer;
+    return 0;
+}
