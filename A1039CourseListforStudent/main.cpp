3d3efd340d811a159674c464fd30a08284643035
Jllobvemy <jllobvemy@gmail.com>
2022-08-03 19:25:15 +0800

    A1039

---
+++ A1039CourseListforStudent/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+// Accepted!
+using namespace std;
+
+
+int main() {
+    map<string, vector<int>> stu2course;
+    int N; // number of students who look for their course lists
+    int K; // total number of courses
+    cin >> N >> K;
+    for (int i = 0; i < K; ++i) {
+        int index, num;
+        cin >> index >> num;
+        for (int j = 0; j < num; ++j) {
+            string name;
+            cin >> name;
+            stu2course[name].push_back(index);
+        }
+    }
+    bool firstline = true;
+    for (int i = 0; i < N; ++i) {
+        string name;
+        cin >> name;
+        if (firstline) {
+            firstline = false;
+        }
+        else {
+            cout << endl;
+        }
+        cout << name << " ";
+        auto found = stu2course.find(name);
+        if (found != stu2course.end()) {
+            cout << found->second.size();
+            if (!found->second.empty()) {
+                sort(found->second.begin(), found->second.end());
+                for (auto e: found->second) {
+                    cout << " " << e;
+                }
+            }
+        }
+        else {
+            cout << 0;
+        }
+    }
+    return 0;
+}
