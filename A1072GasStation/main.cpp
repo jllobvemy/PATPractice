f623bbd40cec664772d0dea58bbf97295b2f847c
Jllobvemy <jllobvemy@gmail.com>
2022-08-21 12:21:25 +0800

    A1072

--- A1072GasStation/main.cpp
+++ A1072GasStation/main.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
-// Not Complete.
+// Accepted!
+using namespace std;
 constexpr int MAXLEN = 1050;
 constexpr int INF = INT_MAX / 2 - 1;
-using namespace std;
-array<array<int, MAXLEN>, MAXLEN> G;
-array<int, MAXLEN> d;
-array<bool, MAXLEN> visited;
-array<int, MAXLEN> pre;
 int N, M, K, Ds;
-
+array<array<int, MAXLEN>, MAXLEN> G;
+array<bool, MAXLEN> visited;
+array<vector<int>, MAXLEN> Pre;
+array<int, MAXLEN> d;
 void Init() {
     for (int i = 0; i < MAXLEN; ++i) {
-        d[i] = INF;
         visited[i] = false;
-        pre[i] = i;
+        d[i] = INF;
         for (int j = 0; j < MAXLEN; ++j) {
             G[i][j] = INF;
         }
@@ -21,124 +19,93 @@ void Init() {
 }
 void Reset() {
     for (int i = 0; i < MAXLEN; ++i) {
-        d[i] = INF;
         visited[i] = false;
-        pre[i] = i;
+        d[i] = INF;
+        Pre[i].clear();
     }
 }
 
 void Dijkstra(int n) {
     Reset();
     d[n] = 0;
-    for (int i = 1; i <= N + M; ++i) {
-        int u = -1, minLen = INF;
-        for (int j = 1; j <= N + M; ++j) {
-            if (!visited[j] && d[j] < minLen) {
-                u = j; minLen = d[j];
+    int TotalLocation = N + M + 10;
+    for (int i = 0; i < TotalLocation; ++i) {
+        int u = -1, minDis = INF;
+        for (int j = 1; j <= TotalLocation; ++j) {
+            if (!visited[j] && d[j] != INF && d[j] < minDis) {
+                minDis = d[j];
+                u = j;
             }
         }
         if (u == -1) return;
         visited[u] = true;
-        for (int v = 1; v <= N + M; ++v) {
+        for (int v = 1; v <= TotalLocation; ++v) {
             if (!visited[v] && G[u][v] != INF) {
                 if (G[u][v] + d[u] < d[v]) {
-                    d[v] = d[u] + G[u][v];
-                    pre[v] = u;
+                    d[v] = G[u][v] + d[u];
+                    Pre[v].clear();
+                    Pre[v].push_back(u);
+                }
+                else if (G[u][v] + d[u] == d[v]) {
+                    Pre[v].push_back(u);
                 }
             }
         }
     }
 }
 
-vector<int> PathTmp;
-void DFS(int n) {
-    if (pre[n] == n) {
-        PathTmp.push_back(n);
-        return;
-    }
-    PathTmp.push_back(n);
-    DFS(pre[n]);
-}
-
-vector<int> GetPath(int n) {
-    PathTmp.clear();
-    DFS(n);
-    std::reverse(PathTmp.begin(), PathTmp.end());
-    return PathTmp;
-}
-
 
 int main() {
     Init();
     cin >> N >> M >> K >> Ds;
+    string s1, s2;
+    int dis;
     for (int i = 0; i < K; ++i) {
-        string s1, s2;
-        int dis;
-        int is1, is2;
         cin >> s1 >> s2 >> dis;
-        if (s1[0] == 'G') {
-            is1 = N + atoi(s1.c_str() + 1);
+        int p1, p2;
+        if (s1[0] != 'G') {
+            p1 = stoi(s1) + 10;
         }
-        else is1 = atoi(s1.c_str());
-        if (s2[0] == 'G') {
-            is2 = N + atoi(s2.c_str() + 1);
+        else {
+            p1 = stoi(s1.substr(1, s1.length() - 1));
         }
-        else is2 = atoi(s2.c_str());
-        G[is1][is2] = dis;
-        G[is2][is1] = dis;
+        if (s2[0] != 'G') {
+            p2 = stoi(s2) + 10;
+        }
+        else {
+            p2 = stoi(s2.substr(1, s2.length() - 1));
+        }
+        G[p1][p2] = dis;
+        G[p2][p1] = dis;
     }
-    int maxSumDis = 0;
-    map<int, int> minSumDis;
-    map<int, int> minEachDis;
-    set<int> ans0;
-    for (int i = N + 1; i <= M + N; ++i) {
-        int minDis = INF;
-        int sum = 0;
+    int minDis = -1;
+    double minAveDis;
+    int selectGas;
+    for (int i = 1; i <= M; ++i) {
         Dijkstra(i);
-        for (int j = 1; j <= N + M; ++j) {
-            if (minDis > d[j]) {
-                minDis = d[j];
-                minEachDis[i] = minDis;
-            }
-            sum += d[j];
+        auto mm = minmax_element(d.begin() + 11, d.begin() + N + 11);
+        if (*mm.second > Ds) continue;
+        if (*mm.first > minDis) {
+            selectGas = i;
+            minDis = *mm.first;
+            minAveDis = accumulate(d.begin() + 11, d.begin() + N + 11, 0) * 1.0 / N;
         }
-        minSumDis[i] = sum;
-        if (maxSumDis < sum) {
-            maxSumDis = sum;
-            ans0.clear();
-            ans0.insert(i);
-        }
-        else if (maxSumDis == sum) {
-            ans0.insert(i);
-        }
-    }
-
-    bool hasAnswer = false;
-    for (auto& e: minEachDis) {
-        if (e.second < Ds) {
-            hasAnswer = true;
-            break;
-        }
-    }
-    if (!hasAnswer) {
-        cout << "No Solution" << endl;
-        return 0;
-    }
-    set<int> ans;
-    int minDis = INF;
-    if (ans0.size() > 1) {
-        for (auto i: ans0) {
-            if (minSumDis[i] < minDis) {
-                minDis = minSumDis[i];
-                ans.clear();
-                ans.insert(i);
-            }
-            else if (minSumDis[i] == minDis) {
-                ans.insert(i);
+        else if (*mm.first == minDis) {
+            double tmpAve = accumulate(d.begin() + 11, d.begin() + N + 11, 0) * 1.0 / N;
+            if (tmpAve < minAveDis) {
+                minAveDis = tmpAve;
+                selectGas = i;
             }
         }
     }
-    cout << "G" << *ans.begin() - N << endl;
-    cout << minEachDis[*ans.begin()] << " " << minSumDis[*ans.begin()] * 1.0 / N << endl;
+    char buf[50];
+    if (minDis == -1) {
+        sprintf(buf, "No Solution");
+    }
+    else {
+        int tmp = round(minAveDis * 10);
+        sprintf(buf, "G%d\n%.1f %.1f", selectGas, minDis * 1.0, tmp * 1.0 / 10);
+    }
+    cout << buf;
     return 0;
 }
