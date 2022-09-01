d186d65f9e4f0ceb01a87e496ec6ed9b6772cf48
jllobvemy <jllobvemy@gmail.com>
2022-09-01 11:39:53 +0800

    A1161

--- A1161MergingLinkedLists/main.cpp
+++ A1161MergingLinkedLists/main.cpp
@@ -1,67 +1,43 @@
 #include <bits/stdc++.h>
-// Not Ac
+// Accepted!
 using namespace std;
-constexpr int MAXLEN = 200010;
+constexpr int MAXLEN = 100000;
+vector<int> l1, l2, ans;
 struct Node {
-    double order = DBL_MAX;
-    int data = -1;
-    int index = -1;
     int next = -1;
-    bool operator<(const Node& other) const {
-        return order < other.order;
-    }
+    int data;
 };
-array<Node, MAXLEN> List;
+array<Node, MAXLEN> LIST;
+
+
 int main() {
     int start1, start2, N;
     cin >> start1 >> start2 >> N;
     for (int i = 0; i < N; ++i) {
         int index, data, next;
         cin >> index >> data >> next;
-        List[index].data = data;
-        List[index].next = next;
-        List[index].index = index;
+        LIST[index].data = data;
+        LIST[index].next = next;
     }
-    int len1 = 0, len2 = 0;
-    for (int i = start1; i != -1; i = List[i].next) {
-        len1++;
+    for (int i = start1; i != -1; i = LIST[i].next) {
+        l1.push_back(i);
     }
-    for (int i = start2; i != -1; i = List[i].next) {
-        len2++;
+    for (int i = start2; i != -1; i = LIST[i].next) {
+        l2.push_back(i);
     }
-    int cnt = 0;
-    stack<double> q;
-    int i1, i2;
-    if (len1 > len2) {
-        i1 = start1;
-        i2 = start2;
+    if (l2.size() > l1.size()) l1.swap(l2);
+    for (int i = 0, j = l2.size() - 1; i < l1.size(); ++i) {
+        ans.push_back(l1[i]);
+        if (i % 2 == 1 && j >= 0) ans.push_back(l2[j--]);
     }
-    else {
-        i1 = start2;
-        i2 = start1;
-    }
-    for (; i1 != -1; i1 = List[i1].next) {
-        cnt++;
-        List[i1].order = cnt;
-        if (cnt % 2 == 0) {
-            q.push(cnt + .5);
-        }
-    }
-    for (; i2 != -1; i2 = List[i2].next) {
-        List[i2].order = q.top();
-        q.pop();
-    }
-    sort(List.begin(), List.end());
     char buf[100];
-    int len = len1 + len2;
-    for (int i = 0; i < len; ++i) {
-        if (i != len - 1) {
-            sprintf(buf, "%05d %d %05d\n", List[i].index, List[i].data, List[i + 1].index);
-        }
-        else {
-            sprintf(buf, "%05d %d -1\n", List[i].index, List[i].data);
-        }
+    for (int i = 0; i < ans.size(); ++i) {
+        if (i != ans.size() - 1)
+            sprintf(buf, "%05d %d %05d\n", ans[i], LIST[ans[i]].data, ans[i + 1]);
+        else
+            sprintf(buf, "%05d %d -1\n", ans[i], LIST[ans[i]].data);
         cout << buf;
     }
+
     return 0;
 }
