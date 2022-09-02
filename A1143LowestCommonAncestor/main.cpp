91c794388f452cd67041dfa1bf4049d0ab5004d1
jllobvemy <jllobvemy@gmail.com>
2022-09-02 14:42:13 +0800

    A1143

--- A1143LowestCommonAncestor/main.cpp
+++ A1143LowestCommonAncestor/main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// Not ac.
+// Accepted!
 using namespace std;
 struct Node {
     Node* left = nullptr;
@@ -10,12 +10,14 @@ struct Node {
 
 vector<int> PreOrder;
 vector<int> InOrder;
+unordered_map<int, Node*> value2node;
 
 Node* Create(Node* parent, int PreLeft, int PreRight, int InLeft, int InRight) {
     if (PreLeft > PreRight) return nullptr;
     Node* ret = new Node;
     ret->data = PreOrder[PreLeft];
     ret->parent = parent;
+    value2node[ret->data] = ret;
     if (PreLeft == PreRight) return ret;
     int cnt = 0;
     while (PreOrder[PreLeft] != InOrder[InLeft + cnt]) cnt++;
@@ -23,45 +25,28 @@ Node* Create(Node* parent, int PreLeft, int PreRight, int InLeft, int InRight) {
     ret->right = Create(ret, PreLeft + cnt + 1, PreRight, InLeft + cnt + 1, InRight);
     return ret;
 }
-map<int, int> value2level;
-map<int, Node*> value2node;
 
-void LevelOrder(Node* root) {
-    queue<Node*> q;
-    q.push(root);
-    int level = 0;
-    while (!q.empty()) {
-        int size = q.size();
-        level++;
-        while (size--) {
-            Node* tmp = q.front();
-            q.pop();
-            value2level[tmp->data] = level;
-            value2node[tmp->data] = tmp;
-            if (tmp->left) q.push(tmp->left);
-            if (tmp->right) q.push(tmp->right);
+int findLCA(int a, int b) {
+    Node* pa = value2node[a];
+    Node* pb = value2node[b];
+    vector<int> aAncestor;
+    vector<int> bAncestor;
+    for (auto i = pa; i != nullptr; i = i->parent) {
+        aAncestor.push_back(i->data);
+    }
+    for (auto i = pb; i != nullptr; i = i->parent) {
+        bAncestor.push_back(i->data);
+    }
+    int ii;
+    if (aAncestor.size() > bAncestor.size()) aAncestor.swap(bAncestor);
+    std::reverse(aAncestor.begin(), aAncestor.end());
+    std::reverse(bAncestor.begin(), bAncestor.end());
+    for (ii = 0; ii < aAncestor.size(); ii++) {
+        if (aAncestor[ii] != bAncestor[ii]) {
+            break;
         }
     }
-}
-
-int findCommonAncestor(int a, int b) {
-    Node* na = value2node[a];
-    Node* nb = value2node[b];
-    vector<int> va;
-    vector<int> vb;
-    while (na) {
-        va.push_back(na->data);
-        na = na->parent;
-    }
-    while (nb) {
-        vb.push_back(nb->data);
-        nb = nb->parent;
-    }
-    int i = 0;
-    while (va[va.size() - i - 1] == vb[vb.size() - i - 1]) {
-        i++;
-    }
-    return va[va.size() - i];
+    return aAncestor[ii - 1];
 }
 
 
@@ -76,34 +61,32 @@ int main() {
     InOrder = PreOrder;
     sort(InOrder.begin(), InOrder.end());
     root = Create(nullptr, 0, N - 1, 0, N - 1);
-    LevelOrder(root);
     char buf[100];
     for (int i = 0; i < M; ++i) {
-        vector<int> a(2);
-        cin >> a[0] >> a[1];
-        auto f0 = value2level.find(a[0]), f1 = value2level.find(a[1]);
-        if (f0 == value2level.end() && f1 != value2level.end()) {
-            sprintf(buf, "ERROR: %d is not found.", a[0]);
+        int a, b;
+        cin >> a >> b;
+        if (value2node[a] == nullptr && value2node[b] != nullptr) {
+            sprintf(buf, "ERROR: %d is not found.\n", a);
         }
-        if (f0 != value2level.end() && f1 == value2level.end()) {
-            sprintf(buf, "ERROR: %d is not found.", a[1]);
+        else if (value2node[a] != nullptr && value2node[b] == nullptr) {
+            sprintf(buf, "ERROR: %d is not found.\n", b);
         }
-        if (f0 == value2level.end() && f1 == value2level.end()) {
-            sprintf(buf, "ERROR: %d and %d are not found.", a[0], a[1]);
+        else if (value2node[a] == nullptr && value2node[b] == nullptr) {
+            sprintf(buf, "ERROR: %d and %d are not found.\n", a, b);
         }
-        if (f0 != value2level.end() && f1 != value2level.end()) {
-            int anc = findCommonAncestor(a[0], a[1]);
-            if (anc == a[0]) {
-                sprintf(buf, "%d is an ancestor of %d.", a[0], a[1]);
+        else {
+            int A = findLCA(a, b);
+            if (A == a) {
+                sprintf(buf, "%d is an ancestor of %d.\n", a, b);
             }
-            else if (anc == a[1]) {
-                sprintf(buf, "%d is an ancestor of %d.", a[1], a[0]);
+            else if (A == b) {
+                sprintf(buf, "%d is an ancestor of %d.\n", b, a);
             }
             else {
-                sprintf(buf, "LCA of %d and %d is %d.", a[0], a[1], anc);
+                sprintf(buf, "LCA of %d and %d is %d.\n", a, b, A);
             }
         }
-        cout << buf << endl;
+        cout << buf;
     }
     return 0;
 }
